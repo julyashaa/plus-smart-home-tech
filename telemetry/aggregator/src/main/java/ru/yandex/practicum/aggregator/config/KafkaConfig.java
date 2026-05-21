89324e2601af760e7c3c16c74841eab2b128826d
Юлия Родионова <seysspovered@yandex.ru>
2026-05-21 22:05:32 +0300

    feat: add spring cloud config server

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaConfig.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.aggregator.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -7,7 +9,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.aggregator.deserializer.SensorEventDeserializer;
@@ -16,16 +18,19 @@ import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import java.util.Properties;
 
 @Configuration
+@ConfigurationProperties(prefix = "aggregator.kafka")
+@Getter
+@Setter
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+    private String groupId;
 
     @Bean
     public KafkaConsumer<String, SensorEventAvro> consumer() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
